d558b83d27622fe3fb703a75d47fb447d89e50ba
Diego <diegoguatedb2002@gmail.com>
2023-05-10 17:25:24 -0600

    Carga de solicitudes de orden

--- src/pages/AdminView/OrdersList/OrdersList.jsx
+++ src/pages/AdminView/OrdersList/OrdersList.jsx
@@ -3,19 +3,21 @@ import Searcher from '@components/Searcher/Searcher';
 import Order from '@components/Order/Order';
 import NavBar from '@components/NavBar/NavBar';
 import { serverHost } from '@/config';
-import {
-  callFetch, result, loading, error,
-} from '@/hooks/useFetch';
+import useFetch from '@hooks/useFetch';
+import moment from 'moment';
 import styles from './OrdersList.module.css';
+import useToken from '../../../hooks/useToken';
+import Spinner from '../../../components/Spinner/Spinner';
 
 function OrdersList() {
-  const getOrders = async () => {
-    const response = callFetch(`${serverHost}/orderRequest`);
-  };
+  const token = useToken();
+  const {
+    callFetch, result, error, loading,
+  } = useFetch();
 
   useEffect(() => {
-
-  }, [result, loading, error]);
+    callFetch({ uri: `${serverHost}/orderRequest`, headers: { authorization: token } });
+  }, []);
 
   return (
     <div className={`${styles.OrdersList}`}>
@@ -28,9 +30,16 @@ function OrdersList() {
           <Searcher className={`${styles.search}`} />
         </div>
         <div className={`${styles.orders}`}>
-          <Order cliente="Fundación Kinal" fechaSolicitada="24/02/2023" descripcion="5 playeras talla S" />
-          <Order cliente="Fundación Kinal" fechaSolicitada="24/02/2023" descripcion="5 playeras talla S" />
-          <Order cliente="Fundación Kinal" fechaSolicitada="24/02/2023" descripcion="5 playeras talla S" />
+          {error && 'Ocurrió un error.'}
+          {loading && <Spinner />}
+          {result?.length > 0
+            && result.map((val) => (
+              <Order
+                cliente={val.customerName}
+                fechaSolicitada={moment(val.datePlaced).format('DD-MM-YYYY')}
+                descripcion={val.description}
+              />
+            ))}
         </div>
       </main>
     </div>
