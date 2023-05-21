563b103f5d52b825f0f7b54421287cbffa24cfce
Diego <diegoguatedb2002@gmail.com>
2023-05-21 12:54:07 -0600

    Componentes input agregados

---
+++ src/components/TextArea/TextArea.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import randomId from '@helpers/randomString';
+import styles from './TextArea.module.css';
+
+function TextArea({
+  title, error, value, onChange, name, ...props
+}) {
+  const id = randomId(15);
+  return (
+    <div className={`${styles.inputTextContainer} ${error ? styles.error : ''}`}>
+      <label htmlFor={id}>
+        {title}
+      </label>
+      <textarea type="text" {...props} id={id} name={name} defaultValue={value} onChange={onChange} />
+      {error && <div className={styles.inputError}>{error}</div>}
+    </div>
+  );
+}
+
+TextArea.propTypes = {
+  title: PropTypes.string.isRequired,
+  error: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string,
+  name: PropTypes.string,
+};
+
+TextArea.defaultProps = {
+  error: null,
+  value: '',
+  name: randomId(15),
+};
+
+export default TextArea;
