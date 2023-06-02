fae5b57e203b5e2f237084ebd56bfb2c19a93a0b
Diego <diegoguatedb2002@gmail.com>
2023-06-02 11:26:02 -0600

    Correcciones para integrar funcionalidades

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
+import { useNavigate } from 'react-router';
 import styles from './Order.module.css';
 
-function Order({ cliente, fechaSolicitada, descripcion }) {
+function Order({
+  id, cliente, fechaSolicitada, descripcion,
+}) {
+  const navigate = useNavigate();
   return (
     <div className={`${styles.order}`}>
       <div className={`${styles.info}`}>
@@ -21,13 +25,14 @@ function Order({ cliente, fechaSolicitada, descripcion }) {
         </div>
       </div>
       <div className={`${styles.button}`}>
-        <ArrowCircleRightOutlinedIcon style={{ fontSize: '3em' }} />
+        <ArrowCircleRightOutlinedIcon style={{ fontSize: '3em' }} onClick={() => navigate(`/orden/${id}`)} />
       </div>
     </div>
   );
 }
 
 Order.propTypes = {
+  id: PropTypes.string.isRequired,
   cliente: PropTypes.string.isRequired,
   fechaSolicitada: PropTypes.string.isRequired,
   descripcion: PropTypes.string.isRequired,
