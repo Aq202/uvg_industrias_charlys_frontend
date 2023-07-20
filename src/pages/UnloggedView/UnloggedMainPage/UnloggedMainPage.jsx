a4bc143d47af200f52b9c3ca48cfa10494d246e1
Diego <diegoguatedb2002@gmail.com>
2023-07-20 09:18:07 -0600

    Vista para completar registro de cliente

--- src/pages/UnloggedView/UnloggedMainPage/UnloggedMainPage.jsx
+++ src/pages/UnloggedView/UnloggedMainPage/UnloggedMainPage.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import LoginPage from '@pages/UnloggedView/LoginPage';
 import NewOrderRequest from '@pages/UnloggedView/NewOrderRequest';
 import Portfolio from '@pages/Portfolio';
+import FinishRegistrationPage from '../FinishRegistrationPage/FinishRegistrationPage';
 
 function UnloggedMainPage() {
   return (
@@ -11,6 +12,7 @@ function UnloggedMainPage() {
       <Route path="/*" element={<Portfolio />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/newOrder" element={<NewOrderRequest />} />
+      <Route path="/registro" element={<FinishRegistrationPage />} />
     </Routes>
   );
 }
