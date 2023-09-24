feab778234e9aa749d9f7b38ff2f4e8900fb509c
Diego <diegoguatedb2002@gmail.com>
2023-09-24 16:12:43 -0600

    feat: product slider terminado

--- src/hooks/useCount.js
+++ src/hooks/useCount.js
@@ -1,10 +1,17 @@
 import { useState } from 'react';
 
-function useCount(startValue) {
-  const [count, setCount] = useState(startValue || null);
+function useCount(startValue, minValue = null, maxValue = null) {
+  const [count, setCount] = useState(startValue ?? null);
 
-  const next = () => setCount((prev) => (Number.isNaN(parseInt(count, 10)) ? 0 : prev + 1));
-  const previous = () => setCount((prev) => (Number.isNaN(parseInt(count, 10)) ? 0 : prev - 1));
+  const next = () => setCount((prev) => {
+    const nextValue = Number.isNaN(parseInt(count, 10)) ? 0 : prev + 1;
+    return nextValue > maxValue ? prev : nextValue;
+  });
+
+  const previous = () => setCount((prev) => {
+    const previousValue = Number.isNaN(parseInt(count, 10)) ? 0 : prev - 1;
+    return previousValue < minValue ? prev : previousValue;
+  });
 
   return {
     count, next, previous, setCount,
