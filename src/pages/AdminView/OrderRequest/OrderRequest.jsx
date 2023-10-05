6c08e7a10113f1e4514397f483ad292963e26424
eliasalvarado <77988653+eliasalvarado@users.noreply.github.com>
2023-10-05 15:27:25 -0600

    Arreglos
    Se arregló el funcionamiento de la página según el rol del usuario loggeado

--- src/pages/AdminView/OrderRequest/OrderRequest.jsx
+++ src/pages/AdminView/OrderRequest/OrderRequest.jsx
@@ -1,20 +1,23 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router';
 import moment from 'moment';
 import { serverHost } from '@/config';
 import useFetch from '@hooks/useFetch';
 import useToken from '@hooks//useToken';
 import usePopUp from '@hooks/usePopUp';
+import getTokenPayload from '@helpers/getTokenPayload';
+import consts from '@helpers/consts';
 import ImageViewer from '@components/ImageViewer/ImageViewer';
-import SubLoadingView from '@components/SubLoadingView/SubLoadingView';
+import LoadingView from '@components/LoadingView';
 import ProvisionalClient from '@components/ProvisionalClient/ProvisionalClient';
 import Button from '@components/Button/Button';
 import Products from '@components/Products/Products';
 import Spinner from '@components/Spinner/Spinner';
 import SuccessNotificationPopUp from '@components/SuccessNotificationPopUp/SuccessNotificationPopUp';
 import ErrorNotificationPopUp from '@components/ErrorNotificationPopUp/ErrorNotificationPopUp';
+import NotFoundPage from '@pages/NotFoundPage/NotFoundPage';
 
 import { scrollbarGray } from '@styles/scrollbar.module.css';
 import styles from './OrderRequest.module.css';
@@ -26,6 +29,7 @@ function OrderRequest() {
   const navigate = useNavigate();
   const { orderId } = useParams();
   const token = useToken();
+  const [userRole, setUserRole] = useState(null);
 
   const {
     callFetch: postOrder, result: resultPost, error: errorPost, loading: loadingPost,
@@ -55,6 +59,11 @@ function OrderRequest() {
 
   useEffect(() => {
     if (!orderId && !token) return;
+
+    const tokenUserData = getTokenPayload(token);
+
+    if (tokenUserData?.role) setUserRole(tokenUserData?.role);
+
     callFetch({
       uri: `${serverHost}/orderRequest/${orderId}`,
       headers: {
@@ -79,104 +88,114 @@ function OrderRequest() {
         <div className={`${styles.top}`}>
           <span className={`${styles.title}`}>Solicitud de pedido</span>
         </div>
-        {result?.temporaryClient
+        {result?.temporaryClient && userRole !== consts.role.client
             && (
               <ProvisionalClient
                 orderId={orderId}
                 clientInfo={result?.temporaryClient}
               />
             )}
-        <div className={`${styles.details}`}>
-          {error && 'Ocurrió un error.'}
-          <div className={`${styles.orderInfoContainer}`}>
-            {loading && <SubLoadingView />}
+        {error && <NotFoundPage />}
+        {!error && !loading
+          && (
+          <div className={`${styles.details}`}>
+            <div className={`${styles.orderInfoContainer}`}>
+              {loading && <LoadingView />}
 
-            <div className={`${styles.orderInfoHeader}`}>
-              <div className={`${styles.editOrderContainer}`}>
-                <Button
-                  className={styles.editOrderButton}
-                  type="submit"
-                  text="Editar solicitud de pedido"
-                  name="denyOrder"
-                  onClick={handleEdit}
-                />
-              </div>
-              {result?.clientOrganization
-                && (
-                  <div className={styles.clientInfoContainer}>
-                    <strong>Cliente: </strong>
-                    {result?.clientOrganization.name}
+              <div className={`${styles.orderInfoHeader}`}>
+                {userRole !== consts.role.client
+                  && (
+                  <div className={`${styles.editOrderContainer}`}>
+                    <Button
+                      className={styles.editOrderButton}
+                      type="submit"
+                      text="Editar solicitud de pedido"
+                      name="denyOrder"
+                      onClick={handleEdit}
+                      disabled={!result?.clientOrganization}
+                    />
                   </div>
+                  )}
+                {result?.clientOrganization
+                  && (
+                    <div className={styles.clientInfoContainer}>
+                      <strong>Cliente: </strong>
+                      {result?.clientOrganization.name}
+                    </div>
+                  )}
+                <div className={styles.headerContainer}>
+                  <p>
+                    <strong>Código: </strong>
+                    {result?.id}
+                  </p>
+                  <p>
+                    <strong>Fecha solicitada: </strong>
+                    {moment(result?.datePlaced).format('DD-MM-YYYY')}
+                  </p>
+                </div>
+              </div>
+              <div className={`${styles.detalles}`}>
+                <strong>Detalles: </strong>
+                <p>{result?.description}</p>
+              </div>
+              <div className={`${styles.filesContainer}`}>
+                <h3 className={styles.sectionTitle}>Archivos adjuntos</h3>
+                <div className={`${styles.divFile} ${scrollbarGray}`}>
+                  {result?.media ? (
+                    <div className={styles.imageViewerContainer}>
+                      <ImageViewer images={result?.media} />
+                    </div>
+                  ) : (
+                    <p className={styles.noImagesMessage}>No hay recursos multimedia adjuntos.</p>
+                  )}
+                </div>
+              </div>
+              <div className={`${styles.aditionalDetailsContainer}`}>
+                <h3 className={styles.sectionTitle}>Detalles Adicionales</h3>
+                {result?.details ? result?.details : 'Sin detalles adicionales.'}
+              </div>
+              <div className={`${styles.productsContainer}`}>
+                <div className={`${styles.productsContainerHeader}`}>
+                  <h3 className={styles.sectionTitle}>Productos a realizar</h3>
+                </div>
+                {result?.detail && result?.detail.length > 0 ? (
+                  <Products
+                    products={
+                      result.detail.map((item) => ({ ...item, organization: result.clientOrganization.name || '' }))
+                    }
+                  />
+                )
+                  : (
+                    <div className={`${styles.noProductsMessage}`}>
+                      No hay productos seleccionados para esta solicitud de orden.
+                    </div>
+                  )}
+              </div>
+              <div className={`${styles.deliveryInfoContainer}`}>
+                <div className={`${styles.deliveryDate}`}>
+                  <h3 className={styles.sectionTitle}>Fecha de entrega</h3>
+                  {result?.deadline ? moment(result?.deadline).format('DD-MM-YYYY') : 'Fecha sin asignar'}
+                </div>
+                <div className={`${styles.deliveryCost}`}>
+                  <h3 className={styles.sectionTitle}>Cotización inicial</h3>
+                  {result?.price ? result?.price : 'No tiene cotización'}
+                </div>
+              </div>
+            </div>
+            {userRole !== consts.role.client
+              && (
+              <div className={`${styles.bottom}`}>
+                {!loadingPost && (
+                <div className={`${styles.bottomForm}`}>
+                  <Button className={styles.denyOrderButton} type="submit" text="Rechazar pedido" name="denyOrder" secondary onClick={handleSubmit} />
+                  <Button className={styles.aceptOrderButton} type="submit" text="Iniciar pedido" name="aceptOrder" onClick={handleSubmit} />
+                </div>
                 )}
-              <div className={styles.headerContainer}>
-                <p>
-                  <strong>Código: </strong>
-                  {result?.id}
-                </p>
-                <p>
-                  <strong>Fecha solicitada: </strong>
-                  {moment(result?.datePlaced).format('DD-MM-YYYY')}
-                </p>
+                {loadingPost && <Spinner />}
               </div>
-            </div>
-            <div className={`${styles.detalles}`}>
-              <strong>Detalles: </strong>
-              <p>{result?.description}</p>
-            </div>
-            <div className={`${styles.filesContainer}`}>
-              <h3 className={styles.sectionTitle}>Archivos adjuntos</h3>
-              <div className={`${styles.divFile} ${scrollbarGray}`}>
-                {result?.media ? (
-                  <div className={styles.imageViewerContainer}>
-                    <ImageViewer images={result?.media} />
-                  </div>
-                ) : (
-                  <p className={styles.noImagesMessage}>No hay recursos multimedia adjuntos.</p>
-                )}
-              </div>
-            </div>
-            <div className={`${styles.aditionalDetailsContainer}`}>
-              <h3 className={styles.sectionTitle}>Detalles Adicionales</h3>
-              {result?.details ? result?.details : 'Sin detalles adicionales.'}
-            </div>
-            <div className={`${styles.productsContainer}`}>
-              <div className={`${styles.productsContainerHeader}`}>
-                <h3 className={styles.sectionTitle}>Productos a realizar</h3>
-              </div>
-              {result?.detail && result?.detail.length > 0 ? (
-                <Products
-                  products={
-                    result.detail.map((item) => ({ ...item, organization: result.clientOrganization.name || '' }))
-                  }
-                />
-              )
-                : (
-                  <div className={`${styles.noProductsMessage}`}>
-                    No hay productos seleccionados para esta solicitud de orden.
-                  </div>
-                )}
-            </div>
-            <div className={`${styles.deliveryInfoContainer}`}>
-              <div className={`${styles.deliveryDate}`}>
-                <h3 className={styles.sectionTitle}>Fecha de entrega</h3>
-                {result?.deadline ? moment(result?.deadline).format('DD-MM-YYYY') : 'Fecha sin asignar'}
-              </div>
-              <div className={`${styles.deliveryCost}`}>
-                <h3 className={styles.sectionTitle}>Cotización inicial</h3>
-                {result?.price ? result?.price : 'No tiene cotización'}
-              </div>
-            </div>
+              )}
           </div>
-          <div className={`${styles.bottom}`}>
-            {!loadingPost && (
-            <div className={`${styles.bottomForm}`}>
-              <Button className={styles.denyOrderButton} type="submit" text="Rechazar pedido" name="denyOrder" secondary onClick={handleSubmit} />
-              <Button className={styles.aceptOrderButton} type="submit" text="Iniciar pedido" name="aceptOrder" onClick={handleSubmit} />
-            </div>
-            )}
-            {loadingPost && <Spinner />}
-          </div>
-        </div>
+          )}
       </main>
       <SuccessNotificationPopUp
         title="Listo"
